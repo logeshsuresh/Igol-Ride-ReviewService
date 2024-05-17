396abe093f03a7a5cc8f8571f19219037053f192
Logesh Suresh <logeshsrinivasans@gmail.com>
2024-05-18 00:09:36 +0530

    basic spring data jpa config done

---
+++ src/main/java/com/example/IgolReviewService/IgolReviewServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.IgolReviewService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgolReviewServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgolReviewServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/IgolReviewService/IgolReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IgolReviewService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgolReviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
