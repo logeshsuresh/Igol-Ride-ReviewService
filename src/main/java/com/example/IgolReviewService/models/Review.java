b80c31d62f0e1b41470263ed4d8265311736211f
Logesh Suresh <logeshsrinivasans@gmail.com>
2024-05-23 21:38:47 +0530

    Added more columns

--- src/main/java/com/example/IgolReviewService/models/Review.java
+++ src/main/java/com/example/IgolReviewService/models/Review.java
@@ -1,12 +1,32 @@
 package com.example.IgolReviewService.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
 
 @Entity
+@Table(name = "bookingreview")
 public class Review {
 
     @Id // this annotation makes the id property a primary key for the table
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto-increment
     Long id;
+
+    @Column(nullable = false)
+    String content;
+
+    Double rating;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP) // Format of date object to be stored
+    @CreatedDate // This annotation tells spring only handle it object creation
+    Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP) // Format of date object to be stored
+    @LastModifiedDate // This annotation tells spring only handle it for object update
+    Date updatedAt;
 
 }
