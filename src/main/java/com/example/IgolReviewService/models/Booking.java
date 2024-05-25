3b8400f0d7188e4d656d172ad7331d868910f45f
Logesh Suresh <logeshsrinivasans@gmail.com>
2024-05-26 00:23:44 +0530

    Added booking associated with to review

---
+++ src/main/java/com/example/IgolReviewService/models/Booking.java
@@ -0,0 +1,30 @@
+package com.example.IgolReviewService.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Booking extends BaseModel {
+
+    @OneToOne(cascade = { CascadeType.PERSIST, CascadeType.REMOVE })
+    private Review review; // 1 : 1 relationship b/w Booking and Review
+
+    @Enumerated(value = EnumType.STRING)
+    private BookingStatus bookingStatus;
+
+    @Temporal(value = TemporalType.TIMESTAMP)
+    private Date startTime;
+
+    @Temporal(value = TemporalType.TIMESTAMP)
+    private Date endTime;
+
+    private Long totalDistance;
+
+}
